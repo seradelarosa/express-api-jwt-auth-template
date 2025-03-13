748e2de112f3fcd330a85dc5d2a9bd47f7663df5
Sera De La Rosa <sera.delarosa@outlook.com>
2025-03-13 12:53:45 -0400

    Sign up route finished.

---
+++ controllers/auth.js
@@ -0,0 +1,33 @@
+// /controllers/auth.js
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+
+const saltRounds = 12;
+
+router.post('/sign-up', async (req, res) => {
+  try {
+    const userInDatabase = await User.findOne({ username: req.body.username });
+
+    if (userInDatabase) {
+        return res.status(409).json({ error: 'Username already taken.' });
+    }
+
+    const user = await User.create({
+        username: req.body.username,
+        hashedPassword: bcrypt.hashSync(req.body.password, saltRounds)
+    });
+
+    const payload = { username: user.username, _id: user._id };
+    const token = jwt.sign({ payload }, process.env.JWT_SECRET);
+
+    res.status(201).json({ token });
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+module.exports = router;
